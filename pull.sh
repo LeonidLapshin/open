7f9e0e76f5c4bb38ce2624f4ec904e22299a5d51
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2021-04-01 13:01:47 +0300

    blabla

--- pull.sh
+++ pull.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
-count=1000
+sudo apt-get install tcpdump -y
+count=1
 while [ $count -gt 0 ]
 do
+sudo tcpdump -v -i eth0 & 
 docker pull eusacrtest.azurecr.io/rabbitmqtest
+ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | xargs -r kill -9 
 docker image rm eusacrtest.azurecr.io/rabbitmqtest
 count=$[ $count - 1 ]
 done
