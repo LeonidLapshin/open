ed00a35638e62cd341969aad43d95afbbc286887
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2021-05-05 14:10:08 +0300

    test user

--- pull.sh
+++ pull.sh
@@ -3,12 +3,12 @@ sudo apt-get install tcpdump -y
 echo "==="
 ip a
 echo "==="
+sudo tcpdump -v -i eth0 port 443 & 
 count=100
 while [ $count -gt 0 ]
 do
-sudo tcpdump -v -i eth0 & 
 docker pull eusacrtest.azurecr.io/rabbitmqtest
-ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | xargs -r kill -9 
 docker image rm eusacrtest.azurecr.io/rabbitmqtest
 count=$[ $count - 1 ]
 done
+ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | xargs -r kill -9 | exit 0
