543a9323c5d0663213c196a07a03aef72b111f72
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2021-05-26 11:43:21 +0300

    add pcap

--- pull.sh
+++ pull.sh
@@ -3,7 +3,7 @@ sudo apt-get install tcpdump -y
 echo "==="
 ip a
 echo "==="
-sudo tcpdump -v -i eth0 port 443 & 
+sudo tcpdump -v -i eth0 port 443 -w cus.pcap & 
 count=100
 while [ $count -gt 0 ]
 do
