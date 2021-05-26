e101afcc7578edff01e1ac07e9de099e6a07aabc
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2021-05-26 14:12:19 +0300

    sudo

--- pull.sh
+++ pull.sh
@@ -15,6 +15,6 @@ sudo tcpdump -v -i eth0 port 443 -w cus.pcap &
 docker pull cusacrtest.azurecr.io/rabbitmqtest
 docker image rm cusacrtest.azurecr.io/rabbitmqtest
 count=$[ $count - 1 ]
-ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | xargs -r kill -9
+sudo ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | xargs -r kill -9
 done
-ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | xargs -r kill -9 | exit 0
+sudo ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | xargs -r kill -9 | exit 0
