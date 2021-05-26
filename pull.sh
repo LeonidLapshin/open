9ba0c514d0caded0bd487843b80d4d74b18cd973
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2021-05-26 12:38:21 +0300

    move tcpdump to loop

--- pull.sh
+++ pull.sh
@@ -3,10 +3,11 @@ sudo apt-get install tcpdump -y
 echo "==="
 ip a
 echo "==="
-sudo tcpdump -v -i eth0 port 443 -w cus.pcap & 
 count=100
 while [ $count -gt 0 ]
 do
+rm -rf cus.pcap
+sudo tcpdump -v -i eth0 port 443 -w cus.pcap & 
 docker pull cusacrtest.azurecr.io/rabbitmqtest
 docker image rm cusacrtest.azurecr.io/rabbitmqtest
 count=$[ $count - 1 ]
