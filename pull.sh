66bea6e8242587ca0fbd9ee2e471b51fdf6316c9
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2021-05-26 11:51:56 +0300

    cus with pcap

--- pull.sh
+++ pull.sh
@@ -7,8 +7,8 @@ sudo tcpdump -v -i eth0 port 443 -w cus.pcap &
 count=100
 while [ $count -gt 0 ]
 do
-docker pull eusacrtest.azurecr.io/rabbitmqtest
-docker image rm eusacrtest.azurecr.io/rabbitmqtest
+docker pull cusacrtest.azurecr.io/rabbitmqtest
+docker image rm cusacrtest.azurecr.io/rabbitmqtest
 count=$[ $count - 1 ]
 done
 ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | xargs -r kill -9 | exit 0
