0f26e64075bb4ff4bd21abb7d35134f1a6e0100c
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2021-05-27 11:22:20 +0300

    U tcpdump option, manual error handling

--- pull.sh
+++ pull.sh
@@ -1,20 +1,16 @@
-#!/bin/bash -e
+#!/bin/bash
 sudo apt-get install tcpdump -y
-echo "==="
 ip a
-echo "==="
 count=100
 while [ $count -gt 0 ]
 do
-ls -la
-df -h
 rm -rf cus.pcap
-ls -la
-df -h
-sudo tcpdump -v -i eth0 port 443 -w cus.pcap & 
-docker pull cusacrtest.azurecr.io/rabbitmqtest
-docker image rm cusacrtest.azurecr.io/rabbitmqtest
+sudo tcpdump -U -v -i eth0 port 443 -w cus.pcap & 
+docker pull cusacrtest.azurecr.io/rabbitmqtest || sleep 5; ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | sudo xargs -r sudo kill -2; sleep 5; exit 1
+docker image rm cusacrtest.azurecr.io/rabbitmqtest || sleep 5; ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | sudo xargs -r sudo kill -2; sleep 5; exit 1
 count=$[ $count - 1 ]
-ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | sudo xargs -r sudo kill -9
+sleep 5
+ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | sudo xargs -r sudo kill -2
+sleep 5
 done
 ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | sudo xargs -r sudo kill -9 | exit 0
