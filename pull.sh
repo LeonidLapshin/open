d7dcd4cdfee4d550d86bd9e70c138507d2f29636
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2021-05-27 13:17:28 +0300

    check pull out

--- pull.sh
+++ pull.sh
@@ -9,11 +9,14 @@ do
     sudo tcpdump -U -v -i eth0 port 443 -w cus.pcap & 
 
     pull_out=$(docker pull cusacrtest.azurecr.io/rabbitmqtest)
+    echo "==="
+    echo $pull_out
+    echo "==="
     if [[ $pull_out != "*Downloaded newer image*" ]]; then
         sleep 5
         ps -ef | grep 'tcpdump' | grep -v grep | awk '{print $2}' | sudo xargs -r sudo kill -2
         sleep 5
-        exit 1
+        exit 0
     fi
 
     docker image rm cusacrtest.azurecr.io/rabbitmqtest 
